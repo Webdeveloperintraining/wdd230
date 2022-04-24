4a71230dd30a55d9a25f1e76efb5239cf15b0839
Webdeveloperintraining <sil22006@byui.edu>
2022-04-23 18:39:58 -0700

    JavaScript changes and column gap modifications

--- js/date.js
+++ js/date.js
@@ -1,1 +1,5 @@
-const x= new Date();
+var lastest_year = new Date();
+document.queryselector('.year').innerHTML=years.getFullYear()
+
+var lastest_date = new Date(document.lastmodified);
+document.queryselector('.date').innerHTML=lastest_date;
