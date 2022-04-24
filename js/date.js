292ecb5c2fd51b8d0feff150d18560c3fad20ef9
Webdeveloperintraining <sil22006@byui.edu>
2022-04-23 20:03:03 -0700

    Another attempt to Javascript

--- js/date.js
+++ js/date.js
@@ -1,4 +1,4 @@
 const year = new Date();
-document.querySelector('#year').innerHTML = years.getFullYear()
+document.querySelector('year').innerHTML = year.getFullYear()
 
 document.getElementById('date').innerHTML=new Date(document.lastmodified);
