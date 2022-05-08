1e2e132ab0515f5dafe7dfa00514cff640e0f4a1
Webdeveloperintraining <sil22006@byui.edu>
2022-05-07 20:48:41 -0700

    text  complete

---
+++ lesson3/scripts/date.js
@@ -0,0 +1,6 @@
+let date = document.lastModified;
+document.getElementById("date").innerHTML = date;
+
+const dt= new Date();
+let year=dt.getFullYear();
+document.querySelector('#year').innerHTML=year;
