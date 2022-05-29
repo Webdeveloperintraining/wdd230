8b39fa9d662935bac1b748e78feadd82f26a7a17
Webdeveloperintraining <sil22006@byui.edu>
2022-05-28 17:13:44 -0700

    optimized images and windchill formula

--- chamber/js/banner.js
+++ chamber/js/banner.js
@@ -1,7 +1,9 @@
 let bdate= new Date().getDay();
 const banner=document.querySelector('#banner');
-if (bdate===1||bdate===2) {
+
+if (bdate==1||bdate==2) {
     banner.style.display="block";
+    document.querySelector("#welcome-title").style.top="35%"
 }
 else {
     banner.style.display="none";
---
+++ chamber/js/windChill.js
@@ -0,0 +1,18 @@
+var temperature=parseInt(50);
+var speed=parseFloat(3);
+
+let t=document.querySelector("#temperature");
+let s=document.querySelector("#wind");
+let windChill=document.querySelector("#wind-chill");
+
+const chillFormula=35.74+(0.6215*temperature)-(35.75*speed**0.16)+(0.4275*temperature*speed**0.16);
+
+const toCelsius=(temperature*9/5)+32;
+const toFarenheit=(temperature-32)*5/9;
+
+const mph=speed/1.60934
+const kmH=speed*1.60934
+
+t.innerHTML=`<h2>${temperature} °F</h2>`;
+s.innerHTML=`<p>${speed} mp/h</p>`;
+windChill.innerHTML=`<p>${chillFormula.toFixed(2)} °F</p>`;
