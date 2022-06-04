09fc32157ec81f6d4da7e48ad97bca3635f7c144
Webdeveloperintraining <sil22006@byui.edu>
2022-06-04 01:27:47 -0700

    First wave of diverse adjustments

--- chamber/js/localStorage.js
+++ chamber/js/localStorage.js
@@ -1,1 +1,17 @@
-const visitsDisplay =document.querySelector(".visits");
+const todayDisplay = document.querySelector(".today");
+const visitsDisplay = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+}
+
+numVisits++;
+
+localStorage.setItem("visits-ls", numVisits);
+
+todayDisplay.textContent = Date.now();
