fe018a921dbf55702f37b7f9642a65e9fd99c403
Webdeveloperintraining <sil22006@byui.edu>
2022-06-13 07:47:44 -0700

    images labaled

--- chamber/js/banner.js
+++ chamber/js/banner.js
@@ -3,7 +3,8 @@ const banner=document.querySelector('#banner');
 
 if (bdate==1||bdate==2) {
     banner.style.display="block";
-    document.querySelector("#welcome-title").style.top="35%"
+    document.querySelector("#welcome-title").style.top="15%";
+    document.querySelector("#rio").style.maxHeight="89.9%";
 }
 else {
     banner.style.display="none";
