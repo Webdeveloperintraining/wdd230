97e9a9318c93ccb16eb67befd860e8d0f977bb19
Webdeveloperintraining <sil22006@byui.edu>
2022-06-13 06:29:02 -0700

    Javascript input validator working

--- chamber/js/form.js
+++ chamber/js/form.js
@@ -2,14 +2,12 @@
 let dv= document.lastModified;
 document.querySelector("#full-datev").value=dv;
 
-
 //This function validates if the Business position was submitted properly
 function validate(){
-  const input = document.querySelector('#position-title');
-  let validationRGEX = /^(\w*){8,16}$/;
-  let result = validationRGEX.test(input);
-
-  if(result == false)
+  const input = document.querySelector('.validator').value;
+  let validation = /^[A-Za-z'-\s]{7,}$/;
+  let result = validation.test(input);
+  if(result == false && input!=="")
   {
     alert('Please enter a valid Job position');
     document.querySelector('.validator').style.border="#FFD600 solid 2px";
