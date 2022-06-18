34e0189d26ed237998806719f814cdc69390d1e0
Webdeveloperintraining <sil22006@byui.edu>
2022-06-18 08:58:46 -0700

    Drectory page almost complete

--- chamber/js/business_cards.js
+++ chamber/js/business_cards.js
@@ -16,19 +16,23 @@ function companyInfo(company){
     let phone=document.createElement("p");
     let address=document.createElement("p");
     let website=document.createElement("p");
+    let core=document.createElement("p");
     let membership=document.createElement("p");
     let logo=document.createElement("img");
 
     name.innerHTML=company.name;
-    phone.innerHTML=company.phone;
-    address.innerHTML=company.address;
-    website.innerHTML=`<a href="${company.website}">${company.website}</a>`;
+    phone.innerHTML=`Phone: ${company.phone}`;
+    address.innerHTML=`Address:<br> ${company.address}`;
+    core.innerHTML=`Core Business: ${company.coreBusiness}`;
+    website.innerHTML=`<a href="${company.website}" target="_blank">${company.website}</a>`;
     membership.innerHTML=`Membership level: ${company.membershipLevel}`;
     logo.setAttribute("src",company.icon);
     logo.setAttribute("alt",`${company.name}'s logo`);
 
     section.appendChild(name);
     section.appendChild(logo);
+    section.appendChild(core)
+    section.appendChild(address);
     section.appendChild(phone);
     section.appendChild(website);
     section.appendChild(membership);
---
+++ chamber/js/list_or_grid.js
@@ -0,0 +1,16 @@
+const grid= document.querySelector("grid");
+const list=document.querySelector("list");
+const display = document.querySelector(".contacts");
+
+grid.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
