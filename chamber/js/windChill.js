1766e2b4f6c254807e7ad7463f15490419590fa4
Webdeveloperintraining <sil22006@byui.edu>
2022-06-21 20:54:24 -0700

    Week 10 Assignment complete

--- chamber/js/windChill.js
+++ chamber/js/windChill.js
@@ -22,8 +22,8 @@ getJson()
 
 function displayTemp(Tijuana){
     temp.innerHTML= `<p>${Tijuana.main.temp.toFixed(0)} &deg;F</p>`;
-    wind.innerHTML=`<p>${Tijuana.wind.speed} mPH</p>`;
-    weatherImg.setAttribute("src",`https://openweathermap.org/img/w/${Tijuana.weather[0].icon}.png`);
+    wind.innerHTML=`${Tijuana.wind.speed} mPH`;
+    weatherImg.setAttribute("src",`https://openweathermap.org/img/wn/${Tijuana.weather[0].icon}.png`);
     weatherImg.setAttribute("alt",`${Tijuana.weather[0]}.description`);
     description.innerHTML=`<h3>${Tijuana.weather[0].description}</h3>`;
     temperature=parseInt(Tijuana.main.temp);
@@ -35,11 +35,11 @@ function displayTemp(Tijuana){
 function Chill(){
     const chillFormula=35.74+(0.6215*temperature)-(35.75*wSpeed**0.16)+(0.4275*temperature*wSpeed**0.16);
     if (temperature > 50 || wSpeed < 3.0){
-        windChill.innerHTML="<p>N/A</p>";
+        windChill.innerHTML="N/A";
         //windChill.innerHTML=`<p>${chillFormula.toFixed(2)} °F</p>`;
     } else{
         //windChill.innerHTML="<p>N/A</p>";
-        windChill.innerHTML=`<p>${chillFormula.toFixed(2)} °F</p>`;
+        windChill.innerHTML=`${chillFormula.toFixed(2)} °F`;
     };
 };
 
