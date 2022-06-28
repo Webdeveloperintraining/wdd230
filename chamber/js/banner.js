c459e53ff1f09102e00408aae3b56a4e7e156af8
Webdeveloperintraining <sil22006@byui.edu>
2022-06-27 21:57:50 -0700

    buttons made homepage fixed

--- chamber/js/banner.js
+++ chamber/js/banner.js
@@ -3,9 +3,13 @@ const banner=document.querySelector('#banner');
 
 if (bdate==1||bdate==2) {
     banner.style.display="block";
+    banner.style.marginTop="15px";
     //document.querySelector("#welcome-title").style.top="5%";
     //document.querySelector("#rio").style.maxHeight="89.9%";
 }
 else {
     banner.style.display="none";
+    
+    //document.querySelector('.main-s').style.gridRow="2/3"
+    //document.querySelector('.hero').style.gridRow="2/3"
 };
