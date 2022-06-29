e4c9eac5f0f69a24d6a0977643aa8f2916640637
Webdeveloperintraining <sil22006@byui.edu>
2022-06-29 16:57:08 -0700

    multiple fixes

--- chamber/js/banner.js
+++ chamber/js/banner.js
@@ -1,7 +1,7 @@
 let bdate= new Date().getDay();
 const banner=document.querySelector('#banner');
 
-if (bdate==1||bdate==2) {
+if (bdate==3||bdate==2) {
     banner.style.display="block";
     // banner.style.marginTop="15px";
     //document.querySelector("#welcome-title").style.top="5%";
--- chamber/js/localStorage.js
+++ chamber/js/localStorage.js
@@ -5,9 +5,9 @@ let numVisits = Number(window.localStorage.getItem("visits-ls"));
 
 
 if (numVisits !== 0) {
-	visitsDisplay.textContent = numVisits;
+	visitsDisplay.innerHTML=numVisits;
 } else {
-	visitsDisplay.textContent = `This is your first visit!`;
+	visitsDisplay.innerHTML= "This is your first visit!";
 }
 
 numVisits++;
