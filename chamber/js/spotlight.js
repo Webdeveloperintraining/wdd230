e16209d0aa1262f08c5e619f443cc87fc6da2446
Webdeveloperintraining <sil22006@byui.edu>
2022-06-30 20:09:19 -0700

    random spotlight complete

--- chamber/js/spotlight.js
+++ chamber/js/spotlight.js
@@ -1,18 +1,19 @@
 const requestURL = "https://webdeveloperintraining.github.io/wdd230/chamber/json/data.json";
-
 fetch(requestURL)
 .then(function (response){
     return response.json();
 })
 .then(function (jsonObject){
-    console.table(jsonObject)// temporary checking for valid response and data parsing
     const companies=jsonObject["companies"];
-    company1(companies);
-    company2(companies);
-    company3(companies);
+    const VIPMembers=[];
+    companies.forEach((company)=>{if (company.membershipLevel==="Silver"||company.membershipLevel==="Gold"){VIPMembers.push(companies.indexOf(company))}});
+    function randomIndex (list){ return list[Math.floor(Math.random()*list.length)]};
+    displayContent(companies[randomIndex(VIPMembers)],".spotlight1")
+    displayContent(companies[randomIndex(VIPMembers)],".spotlight2")
+    displayContent(companies[randomIndex(VIPMembers)],".spotlight3")
 });
 
-function company1(company){
+function displayContent(company,spotlight){
     let section=document.createElement("section");
     let namespot=document.createElement("h3");
     let phone=document.createElement("p");
@@ -21,68 +22,17 @@ function company1(company){
     let core=document.createElement("p");
     let logo=document.createElement("img");
 
-    namespot.innerHTML=company[7].name;
-    phone.innerHTML=`${company[7].phone}`;
-    website.innerHTML=`<a href="${company[7].website}" target="_blank">${company[7].website}</a>`;
-    core.innerHTML=`${company[7].coreBusiness}`;
-    logo.setAttribute("src",company[7].icon);
-    logo.setAttribute("alt",`${company[7].name}'s logo`);
+    namespot.innerHTML=company.name;
+    phone.innerHTML=`${company.phone}`;
+    website.innerHTML=`<a href="${company.website}" target="_blank">${company.website}</a>`;
+    core.innerHTML=`${company.coreBusiness}`;
+    logo.setAttribute("src",company.icon);
+    logo.setAttribute("alt",`${company.name}'s logo`);
     section.appendChild(namespot);
     section.appendChild(logo);
     section.appendChild(core)
     section.appendChild(phone);
     section.appendChild(email);
     section.appendChild(website);
-    document.querySelector(".spotlight1").appendChild(section);
-    //document.querySelector(".main-s").appendChild(section);
-};
-
-function company2(company){
-    let section=document.createElement("section");
-    let namespot=document.createElement("h3");
-    let phone=document.createElement("p");
-    let email=document.createElement("p");
-    let website=document.createElement("p");
-    let core=document.createElement("p");
-    let logo=document.createElement("img");
-
-    namespot.innerHTML=company[0].name;
-    phone.innerHTML=`${company[0].phone}`;
-    website.innerHTML=`<a href="${company[0].website}" target="_blank">${company[0].website}</a>`;
-    core.innerHTML=`${company[0].coreBusiness}`;
-    logo.setAttribute("src",company[0].icon);
-    logo.setAttribute("alt",`${company[0].name}'s logo`);
-    section.appendChild(namespot);
-    section.appendChild(logo);
-    section.appendChild(core)
-    section.appendChild(phone);
-    section.appendChild(email);
-    section.appendChild(website);
-    document.querySelector(".spotlight2").appendChild(section);
-    //document.querySelector(".main-s").appendChild(section);
-};
-
-function company3(company){
-    let section=document.createElement("section");
-    let namespot=document.createElement("h3");
-    let phone=document.createElement("p");
-    let email=document.createElement("p");
-    let website=document.createElement("p");
-    let core=document.createElement("p");
-    let logo=document.createElement("img");
-
-    namespot.innerHTML=company[5].name;
-    phone.innerHTML=`${company[5].phone}`;
-    website.innerHTML=`<a href="${company[5].website}" target="_blank">${company[5].website}</a>`;
-    core.innerHTML=`${company[5].coreBusiness}`;
-    logo.setAttribute("src",company[5].icon);
-    logo.setAttribute("alt",`${company[5].name}'s logo`);
-    section.appendChild(namespot);
-    section.appendChild(logo);
-    section.appendChild(core)
-    section.appendChild(phone);
-    section.appendChild(email);
-    section.appendChild(website);
-    //document.querySelector(".main-s").appendChild(section);
-    document.querySelector(".spotlight3").appendChild(section);
+    document.querySelector(spotlight).appendChild(section);
 };
